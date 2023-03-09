f99cf2d8e43844cff280e0028219562e946691f7
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-03-09 11:54:56 -0500

    finished fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { React, useState, useEffect } from 'react'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [articles, setArticles] = useState([])
+  const [subreddit, setSubreddit] = useState("webdev")
+
+  useEffect(() => {
+    fetch ("https://www.reddit.com/r/webdev.json").then (res => {
+      if (res.status != 200) {
+        console.log("Error!")
+        return 
+      }
+      res.json().then(data => {
+        if (data != null) {
+          setArticles(data.data.children)
+        }
+      })
+    })
+
+  }, [subreddit])
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className = "App">
+      <header className = "App-header">
+        <input type = "text" className = "input" value = "webdev" />
+      </header>
+      <article className='articles'>
+
+      </article>
+
     </div>
   )
 }
