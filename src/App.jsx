a1051de54fbb2dd2f603fdd8585057c804fef74c
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-03-10 11:02:02 -0500

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
 
   useEffect(() => {
-    fetch ("https://www.reddit.com/r/webdev.json").then (res => {
+    fetch ("https://www.reddit.com/r/"+ subreddit + ".json").then (res => {
       if (res.status != 200) {
         console.log("Error!")
         return 
@@ -28,12 +28,12 @@ function App() {
   return (
     <div className = "App">
       <header className = "App-header">
-        <input type = "text" className = "input" value = "webdev" />
+        <input type = "text" className = "input" value = {subreddit} onChange = {(e)=> setSubreddit(e.target.value)} />
       </header>
       <article className='articles'>
       </article>
         {
-        (articles != null) ? articles.map((article, index) => <Article key = {index} article = {article.data} />) : ""
+        (articles != null) ? articles.map((article, index) => <Article key = {index} article = {article.data} thumbnail = {article.thumbnail}/>) : ""
         }
 
     </div>
