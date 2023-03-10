a9287ed14ce120db2556226816ae62a6cf6764c5
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-03-10 11:01:55 -0500

    finished

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -5,6 +5,7 @@ export default function Article (props) {
         <article>
             <a href = { "https://reddit.com" + props.article.permalink } target = "_blank"> 
                 <h3>{props.article.title}</h3>
+                <img src = {props.article.thumbnail} height = "300px" width = "300px" />
             </a>
 
         </article>
